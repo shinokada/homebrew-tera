52617897543e96a405f3f00cdb06dacfa6f75965
Shinichi Okada <okada.shin@gmail.com>
2021-07-07 16:02:08 +0900

    version 0.2.10

--- tera.rb
+++ tera.rb
@@ -1,7 +1,7 @@
 class Tera < Formula
     desc "Terminal radio, TERA, plays radio, CRUD favorites, search radio stations. Powered by Radio Browser API and MPV, a free, open source, and cross-platform media player."
-    url "https://github.com/shinokada/tera/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "140ec92b950173a784185b41184b804337752cfc2423bf7dde7e46c0994d473e"
+    url "https://github.com/shinokada/tera/archive/refs/tags/v0.2.10.tar.gz"
+    sha256 "47dd8207e72f99385afc535af3e2daa2f76998d2937d6b5c913d62c374fa84fb"
     license "MIT"
   
     depends_on "mpv"
@@ -9,21 +9,21 @@ class Tera < Formula
     depends_on "jq"
 
     def install
-      bin.install "tera"
-      bin.install "LICENSE"
-      bin.install "README.md"
-      bin.install "delete_station"
-      bin.install "gist"
-      bin.install "lib"
-      bin.install "list"
-      bin.install "lucky"
-      bin.install "play"
-      bin.install "search"
-      bin.install Dir["./images"]
-
+        bin.install Dir["./images"]
+        bin.install "delete_station"
+        bin.install "gist"
+        bin.install "lib"
+        bin.install "LICENSE"
+        bin.install "list"
+        bin.install "lucky"
+        bin.install "play"
+        bin.install "README.md"
+        bin.install "sample.json"
+        bin.install "search"
+        bin.install "tera"
     end
   
     test do
-      system "false"
+        system "false"
     end
   end
