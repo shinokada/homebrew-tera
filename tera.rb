3b845d312a17211b45ae3e7c4537de99cc7c49db
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2023-11-26 14:36:54 +0100

    v0.4.3

--- tera.rb
+++ tera.rb
@@ -1,7 +1,7 @@
 class Tera < Formula
     desc "Terminal radio, TERA, plays radio, CRUD favorites, search radio stations. Powered by Radio Browser API and MPV, a free, open source, and cross-platform media player."
-    url "https://github.com/shinokada/tera/archive/refs/tags/v0.4.2.tar.gz"
-    sha256 "b5bc3c5d560b05a2700b68c65e6f06e056784afc84e8336bc56a68f7584e8edd"
+    url "https://github.com/shinokada/tera/archive/refs/tags/v0.4.3.tar.gz"
+    sha256 "abc77037f423d91501e5c6d8d27298115020501a7dd634bfc87d8b09207b816f"
     license "MIT"
   
     depends_on "mpv"
