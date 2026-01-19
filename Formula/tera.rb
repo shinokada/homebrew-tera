c15724264e36b2135999cd32c93fff80760c1b82
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2026-01-19 15:08:48 +0100

    v0.7.2

--- Formula/tera.rb
+++ Formula/tera.rb
@@ -1,8 +1,8 @@
 class Tera < Formula
   desc "Interactive Bash script terminal music radio player. Play your favorite radio station, CRUD your favorite lists, and explore new radio stations from your terminal. "
   homepage "https://tera.codewithshin.com/"
-  url "https://github.com/shinokada/tera/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "828dc1a61b399fd2131048176b1fb36704bbed7ab7fa06383d5bab2f269c0ab9"
+  url "https://github.com/shinokada/tera/archive/refs/tags/v0.7.2.tar.gz"
+  sha256 "56bbc4d4ea02824268ceb1e932dfe394cfa9ce66ef9f8b18c4f31ee82352fd78"
   license "MIT"
 
   uses_from_macos "bash"
