a0616193a7a1daf49802e69be7f9c5d47acf3c75
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2026-01-29 11:21:04 +0100

    v1.0.0-rc.2

--- Formula/tera.rb
+++ Formula/tera.rb
@@ -6,23 +6,23 @@ class Tera < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/shinokada/tera/releases/download/v1.0.0-rc.1/tera_1.0.0-rc.1_darwin_amd64.tar.gz"
-      sha256 "6ce1d170326355e5a423cc7d551e295a8e248f5841d7332f9d310bc7be620b4f"
+      url "https://github.com/shinokada/tera/releases/download/v1.0.0-rc.2/tera_1.0.0-rc.2_darwin_amd64.tar.gz"
+      sha256 "dc9d409de414dcae271356f15110e39d5a573dd8ab9f467a903c409080591667"
     end
     on_arm do
-      url "https://github.com/shinokada/tera/releases/download/v1.0.0-rc.1/tera_1.0.0-rc.1_darwin_arm64.tar.gz"
-      sha256 "e7b03c0e6b5de25411bb9395e193704f0f824a2792ade090f17963a8d7148214"
+      url "https://github.com/shinokada/tera/releases/download/v1.0.0-rc.2/tera_1.0.0-rc.2_darwin_arm64.tar.gz"
+      sha256 "85f5f25e7d1b55436a288878d8bd8d05ae09b5cf48b7177287eb11b6b21377c5"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/shinokada/tera/releases/download/v1.0.0-rc.1/tera_1.0.0-rc.1_linux_amd64.tar.gz"
-      sha256 "d4e90e3d2cfa5dd9c286da31df86f4afe2b1fed322a6476e142f8cfe49a0f907"
+      url "https://github.com/shinokada/tera/releases/download/v1.0.0-rc.2/tera_1.0.0-rc.2_linux_amd64.tar.gz"
+      sha256 "9e83477d566ee402c15e7e99c23bdbbcd1929bd157409f6e96fc60cb18b85985"
     end
     on_arm do
-      url "https://github.com/shinokada/tera/releases/download/v1.0.0-rc.1/tera_1.0.0-rc.1_linux_arm64.tar.gz"
-      sha256 "5904ef726ee11aecac4e1315410061b16dde38c35679d9c28b6524086e58ba8d"
+      url "https://github.com/shinokada/tera/releases/download/v1.0.0-rc.2/tera_1.0.0-rc.2_linux_arm64.tar.gz"
+      sha256 "c36e30b187bdd6afd711543428b1d70991e038b6ed0d99066d95d804bcb98e5c"
     end
   end
 
